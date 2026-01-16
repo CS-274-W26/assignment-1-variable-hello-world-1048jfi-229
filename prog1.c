868353e7afd99c45c74e90f797690c20045b9265
1048jfi-229 <coleemer@oregonstate.edu>
2026-01-15 17:16:53 -0800

    completed assignment

--- prog1.c
+++ prog1.c
@@ -15,7 +15,19 @@ int main()
 	int myrand = gen_rand(10, 0);
     if (myrand >= 0 && myrand <= 4)
     {
-        printf("Eat more beef, kick less cats."\n)
+        printf("Eat more beef, kick less cats.\n");
     }
-    else if 
+    else if (myrand >= 5 && myrand <= 9)
+    {
+        printf("FRODO LIVES\n");
+    }
+    else if (myrand == 10)
+    {
+        printf("Larn is the best roguelike.\n");
+    }
+    else
+    {
+        printf("something went wrong\n");
+    }
+    printf("The random number was: %d\n", myrand);
 }

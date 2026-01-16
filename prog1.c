4e4afa5e04836a5bb1ede3c70492f13789589718
1048jfi-229 <coleemer@oregonstate.edu>
2026-01-15 17:01:09 -0800

    fire alarm going off

--- prog1.c
+++ prog1.c
@@ -1,11 +1,21 @@
 #include <stdio.h> // Necessary for printf()
+#include <stdlib.h>
+#include <time.h>
 
-// TODO Other includes
+int gen_rand(int upper, int lower)
+{
+    srand(time(NULL));
+    return(rand() % (upper + 1 - lower) + lower);
+}
 
-
-int main() {
-	// The main function is the entrypoint of the program. When the program
+int main()
+{
+    // The main function is the entrypoint of the program. When the program
 	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+	int myrand = gen_rand(10, 0);
+    if (myrand >= 0 && myrand <= 4)
+    {
+        printf("Eat more beef, kick less cats."\n)
+    }
+    else if 
 }
